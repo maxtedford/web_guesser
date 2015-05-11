84cb5d6c4d44b6cf7996f3f2a2a482b52e4b4338
Max Tedford <max.tedford@gmail.com>
2015-05-11 13:17:12 -0400

    I5: cleaning up some of the methods. I may add another extensions, but moving on to the next assignment at this point

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,25 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(101)
-number = SECRET_NUMBER
 
 get "/" do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => number, :message => message}
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
 
 def check_guess(guess)
-  number = SECRET_NUMBER
-  if guess.to_i > number + 5
+  if guess.to_i > SECRET_NUMBER + 5
     "Way too high!"
-  elsif guess.to_i > number && guess.to_i <= number + 5
+  elsif guess.to_i > SECRET_NUMBER && guess.to_i <= SECRET_NUMBER + 5
     "Too high!"
-  elsif guess.to_i < number - 5
+  elsif guess.to_i < SECRET_NUMBER - 5
     "Way too low!"
-  elsif guess.to_i < number && guess.to_i >= number - 5
+  elsif guess.to_i < SECRET_NUMBER && guess.to_i >= SECRET_NUMBER - 5
     "Too low!"
   else
-    "Correct! The secret number is #{number}"
+    "Correct! The secret number is #{SECRET_NUMBER}"
   end
 end
